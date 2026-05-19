fff5fc2b58c8e0fab6ba2a2f290d949ac77f21b0
Cesar Noronha <cesarn.junior2@gmail.com>
2026-05-19 11:52:37 -0300

    docs: documentação detalhada da aba App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,34 @@ import Dashboard from './pages/Dashboard/Dashboard';
 import Cadastro from './pages/Cadastro/Cadastro';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+/* O App.jsx gerencia o estado global de autenticação */
+
 function App() {
-  // Inicialmente null (nenhum usuário logado)
   const [user, setUser] = useState(null); 
 
   const logarUsuario = (dadosDoUsuario) => {
-    // Aqui guardamos o objeto completinho que veio do FastAPI
+    // Objeto do FastApi
     setUser({
       id: dadosDoUsuario.id,
       nome: dadosDoUsuario.usuario
     });
   };
 
+  // Se o usuário for nulo, o usuário fica restrito às rotas públicas de login e cadastro
+  // No caso da tela de  login, a função logarUsuario torna-se uma prop chamada onLogin dentro de Home
+
   if (!user) {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home onLogin={logarUsuario} />} />
+          <Route path="/" element={<Home onLogin={logarUsuario} />} /> 
           <Route path="/cadastro" element={<Cadastro />} />
         </Routes>
       </BrowserRouter>
     );
   } 
 
-  // O SEGREDO ESTÁ AQUI: 
-  //userName precisa receber user.nome (uma string) e NÃO o objeto 'user' inteiro!
+  // Existindo o usuário, a tela de Dashboard é renderizada
   return <Dashboard userName={user.nome} userId={user.id} />;
 }
 
